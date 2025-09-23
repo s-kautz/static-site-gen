8978443a1ae164de2be4b50fedc2e23d994fc581
Steffen Kautz <steffenkautz@Steffens-MacBook-Pro.local>
2025-09-23 14:46:39 +0200

    moved text to html conversion function into textnode.py

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
   TEXT = "text"
@@ -24,3 +25,19 @@ class TextNode():
   
   def __repr__(self):
     return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+  if not text_node.text_type in TextType:
+    raise ValueError("TextNode object with unhandled TextType")
+  if text_node.text_type == TextType.TEXT:
+    return LeafNode(tag=None, value=text_node.text)
+  if text_node.text_type == TextType.BOLD:
+    return LeafNode(tag="b", value=text_node.text)
+  if text_node.text_type == TextType.ITALIC:
+    return LeafNode(tag="i", value=text_node.text)
+  if text_node.text_type == TextType.CODE:
+    return LeafNode(tag="code", value=text_node.text)
+  if text_node.text_type == TextType.LINK:
+    return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+  if text_node.text_type == TextType.IMAGE:
+    return LeafNode(tag="img", value=text_node.text, props={"src": text_node.url, "alt": text_node.text})
