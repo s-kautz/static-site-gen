282138826f2a55ad808714edb75b00a884e16a33
Steffen Kautz <steffen@kautz.design>
2025-10-02 22:37:08 +0200

    added markdown block processing and tests

---
+++ src/block_markdown.py
@@ -0,0 +1,47 @@
+import re
+
+from enum import Enum
+
+class BlockType(Enum):
+  PARAGRAPH = "paragraph"
+  HEADING = "heading"
+  CODE = "code"
+  QUOTE = "quote"
+  UL = "unordered_list"
+  OL = "ordered_list"
+
+def markdown_to_blocks(markdown):
+  block_delimiter = r"(\n\s*\n)"
+  blocks = re.split(pattern=block_delimiter, string=markdown)
+  blocks[:] = [block.strip() for block in blocks if not block.strip() == ""]
+
+  return blocks
+
+def block_to_block_type(mdblock):
+  heading_pattern = r"#{1,6} [\s\S]"
+  code_pattern = r"```[\s\S]*```"
+  quote_pattern = r"> [\s\S]"
+  ul_line_pattern = r"- [\s\S]"
+
+  if re.match(heading_pattern, mdblock):
+    return BlockType.HEADING
+  if re.match(code_pattern, mdblock):
+    return BlockType.CODE
+  if re.match(quote_pattern, mdblock):
+    return BlockType.QUOTE
+  lines = mdblock.split("\n")
+  is_list = True
+  for line in lines:
+    if not re.match(ul_line_pattern, line):
+      is_list = False
+  if is_list:
+    return BlockType.UL
+  is_list = True
+  for i in range(1, len(lines)+1):
+    ol_line_pattern = fr"{i}. "
+    if not re.match(ol_line_pattern, line):
+      is_list = False
+  if is_list:
+    return BlockType.OL
+  return BlockType.PARAGRAPH
+
---
+++ src/test_block_markdown.py
@@ -0,0 +1,121 @@
+import unittest
+
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
+
+class TestTextNode(unittest.TestCase):
+  def test_markdown_to_blocks(self):
+    test_markdown = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+
+"""
+    test_blocks = markdown_to_blocks(test_markdown)
+    self.assertEquals(
+      test_blocks,
+      [
+        "This is **bolded** paragraph",
+        "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+        "- This is a list\n- with items",
+      ],
+    )
+  
+  def test_markdown_to_blocks_leading_and_trailing_spaces(self):
+    test_markdown = """
+This is a regular paragraph.
+
+ This is a paragraph with a leading space
+
+This is a paragraph with a trailing space """
+    test_blocks = markdown_to_blocks(test_markdown)
+    self.assertEquals(
+      test_blocks,
+      [
+        "This is a regular paragraph.",
+        "This is a paragraph with a leading space",
+        "This is a paragraph with a trailing space",
+      ],
+    )
+  
+  def test_block_to_block_type_paragraph(self):
+    mdblock = "This is a regular paragraph"
+    self.assertEquals(block_to_block_type(mdblock), BlockType.PARAGRAPH)
+  
+  def test_block_to_block_type_h1_heading(self):
+    mdblock = "# This is an H1 heading"
+    self.assertEquals(block_to_block_type(mdblock), BlockType.HEADING)
+
+  def test_block_to_block_type_h6_heading(self):
+    mdblock = "###### This is an H1 heading"
+    self.assertEquals(block_to_block_type(mdblock), BlockType.HEADING)
+
+  def test_block_to_block_type_malformat_heading_too_deep(self):
+    mdblock = "####### This is an H7 heading"
+    self.assertEquals(block_to_block_type(mdblock), BlockType.PARAGRAPH)
+
+  def test_block_to_block_type_malformat_heading_no_space(self):
+    mdblock = "#This is an H1 heading with no space"
+    self.assertEquals(block_to_block_type(mdblock), BlockType.PARAGRAPH)
+
+  def test_block_to_block_type_code(self):
+    mdblock = """
+```SomeCode
+This is a test code block
+```
+"""
+    self.assertEquals(block_to_block_type(mdblock), BlockType.CODE)
+
+  def test_block_to_block_type_code(self):
+    mdblock = """
+```SomeCode
+This is an unclosed code block
+"""
+    self.assertEquals(block_to_block_type(mdblock), BlockType.PARAGRAPH)
+
+  def test_block_to_block_type_code(self):
+    mdblock = "> This is a quote"
+    self.assertEquals(block_to_block_type(mdblock), BlockType.QUOTE)
+
+  def test_block_to_block_type_code(self):
+    mdblock = ">This is a malformatted quote"
+    self.assertEquals(block_to_block_type(mdblock), BlockType.PARAGRAPH)
+  
+  def test_block_to_block_type_unordered_list(self):
+    mdblock = """
+- This is an unordered list item
+- And this is another one
+"""
+    self.assertEquals(block_to_block_type(mdblock), BlockType.UL)
+
+  def test_block_to_block_type_unordered_list(self):
+    mdblock = """
+- This is an unordered list item
+-And this is a malformatted one
+"""
+    self.assertEquals(block_to_block_type(mdblock), BlockType.PARAGRAPH)
+  
+  def test_block_to_block_type_ordered_list(self):
+    mdblock = """
+1. This is an ordered list item
+2. And this is another one
+"""
+    self.assertEquals(block_to_block_type(mdblock), BlockType.OL)
+  
+  def test_block_to_block_type_ordered_list(self):
+    mdblock = """
+1. This is an ordered list item
+3. And this is an out of order one
+"""
+    self.assertEquals(block_to_block_type(mdblock), BlockType.PARAGRAPH)
+  
+  def test_block_to_block_type_ordered_list(self):
+    mdblock = """
+1. This is an ordered list item
+2.And this is a malformatted one
+"""
+    self.assertEquals(block_to_block_type(mdblock), BlockType.PARAGRAPH)
+
