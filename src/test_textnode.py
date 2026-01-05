543930ceddc8b4c816c268728de65290563f5e97
= <steffen@kautz.design>
2026-01-05 23:36:34 +0100

    Fixed TextNode to LeafNode conversion to add empty string value to image nodes, added test case for image node conversion

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -23,10 +23,17 @@ class TestTextNode(unittest.TestCase):
     self.assertNotEqual(node1, node2)
   
   def test_text_to_html(self):
-      node = TextNode("This is a text node", TextType.TEXT)
-      html_node = text_node_to_html_node(node)
-      self.assertEqual(html_node.tag, None)
-      self.assertEqual(html_node.value, "This is a text node")
+    node = TextNode("This is a text node", TextType.TEXT)
+    html_node = text_node_to_html_node(node)
+    self.assertEqual(html_node.tag, None)
+    self.assertEqual(html_node.value, "This is a text node")
+  
+  def test_text_to_html_image(self):
+    node = TextNode("A sample image", TextType.IMAGE, "./imagepath/image.jpg")
+    print(node)
+    html_node = text_node_to_html_node(node)
+    print(html_node.to_html())
+    self.assertEqual(html_node.to_html(), f"<img src=\"./imagepath/image.jpg\" alt=\"A sample image\"></img>")
 
 if __name__ == "__main__":
   unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -40,4 +40,4 @@ def text_node_to_html_node(text_node):
   if text_node.text_type == TextType.LINK:
     return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
   if text_node.text_type == TextType.IMAGE:
-    return LeafNode(tag="img", value=text_node.text, props={"src": text_node.url, "alt": text_node.text})
+    return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
