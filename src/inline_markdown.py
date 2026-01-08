7d2058e42e472ef4ef14dd2a3c0fe9bf239805d8
= <steffen@kautz.design>
2026-01-08 20:44:09 +0100

    removed empty lines from inline_markdown.py

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -7,15 +7,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     if old_node.text_type != TextType.TEXT or delimiter not in old_node.text:
       new_nodes.append(old_node)
       continue
-    
     text = old_node.text
-
     if text.count(delimiter) % 2 != 0:
       raise ValueError("ERROR: Delimiter in string not closed")
-    
     apply_text_type = True
     substrings = text.split(delimiter)
-    
     for substring in substrings:
       apply_text_type = not apply_text_type
       if substring == "":
@@ -24,7 +20,6 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.append(TextNode(text=substring, text_type=text_type))
       else:
         new_nodes.append(TextNode(text=substring, text_type=TextType.TEXT))
-  
   return new_nodes
 
 '''
@@ -37,29 +32,23 @@ return matches (empty list if no matches)
 def extract_markdown_images(text):
   images = re.findall(r"!\[.*?\]\(.*?\)", text)
   tuples = []
-
   if not images:
     raise ValueError("ERROR: No image in string")
-  
   for image in images:
     alt_text = re.findall(r"\[.*?\]", image)[0].strip("[]")
     url = re.findall(r"\(.*?\)", image)[0].strip("()")
     tuples.append((alt_text, url))
-  
   return tuples
 
 def extract_markdown_links(text):
   links = re.findall(r"\[.*?\]\(.*?\)", text)
   tuples = []
-  
   if not links:
     raise ValueError("ERROR: No link in string")
-  
   for link in links:
     anchor_text = re.findall(r"\[.*?\]", link)[0].strip("[]")
     url = re.findall(r"\(.*?\)", link)[0].strip("()")
     tuples.append((anchor_text, url))
-  
   return tuples
 
 '''
